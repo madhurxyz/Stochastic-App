7bd726a91e88439b2a1932b4f6d71c9a448cca0d
Madhur Malhotra <malhotra.madhur15@gmail.com>
2016-12-12 23:50:44 -0800

    got count function to get count of the histogram from stochastic.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 from histogram import get_words_list, create_histogram
+from stochastic import get_prob_word, get_count
 
 app = Flask(__name__)
 
 words_list = get_words_list('holmes.txt')
 histogram = create_histogram(words_list)
-print histogram
+count = get_count(histogram)
 
 @app.route('/')
 def word():
