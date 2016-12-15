6ead173416ebc1ec7cd7019771012fd012919282
Madhur Malhotra <malhotra.madhur15@gmail.com>
2016-12-14 23:11:13 -0800

    new styles work

--- main.py
+++ main.py
@@ -32,4 +32,6 @@ if __name__ == '__main__':
     # comment next two lines to run on heroku
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-    # app.run() uncomment to run locally
+
+    # uncomment next line to run locally
+    # app.run()
