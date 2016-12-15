8ced5321cc207fdfcc5efc72b424661152c8763e
Madhur Malhotra <malhotra.madhur15@gmail.com>
2016-12-14 23:07:56 -0800

    changed ports for heroku

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ def word():
 '''
 
 if __name__ == '__main__':
-    # port = int(os.environ.get('PORT', 5000))
-    # app.run(host='0.0.0.0', port=port)
-    app.run()
+    # comment next two lines to run on heroku
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run() uncomment to run locally
